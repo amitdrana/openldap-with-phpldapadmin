5c2e012594372e3799e38a474ae8e71c2c491ec2
root <you@example.com>
2020-01-06 18:52:59 +0530

    ou integrated

--- adduser.sh
+++ adduser.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 user=$1
 pass=$2
+ou=$3
 uid=$(getent passwd | awk -F : '{print $3}' | sort -n |tail -n 2 | head -1)
 uid=$((uid+1))
 
@@ -15,6 +16,8 @@ cp ldapuser.ldif ldapuser1.ldif
 sed -i "s/amit/${user}/g" ldapuser1.ldif
 sed -i "s/{SSHA}p18K5liYx1bdL93SrjztHGApyi3wTkc4/${password}/g" ldapuser1.ldif
 sed -i "s/1000/${uid}/g" ldapuser1.ldif
+sed -i "s/people/${ou}/g" ldapuser1.ldif
+
 
 cat ldapuser1.ldif
 
